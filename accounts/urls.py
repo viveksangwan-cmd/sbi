978d2d1f69432ea8d41faea217b1dc8ac548168e
vivek <viveksangwan006@gmaiil.com>
2020-05-10 23:01:13 +0530

    Account Authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,12 @@
 from django.views.generic.base import TemplateView
 from django.urls import path
-from .views import Balance,debit,credit
+from .views import Balance,Debit,Credit
 from .views import *
 urlpatterns=[
     path('',TemplateView.as_view(template_name='accounts/account.html'),name='account'),
-    path('balance/',balance,name='balance'),
+    path('balance/',Balance.balance,name='balance'),
     path('creditBalance/',TemplateView.as_view(template_name='accounts/credit.html'),name='credit'),
-    path('creditBalance/creditnow',credit,name='creditnow'),
+    path('creditBalance/creditnow',Credit.credit,name='creditnow'),
     path('debitBalance/',TemplateView.as_view(template_name='accounts/debit.html'),name='debit'),
-    path('debitBalance/debitnow',debit,name='debitnow'),
+    path('debitBalance/debitnow',Debit.debit,name='debitnow'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,37 +2,40 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.shortcuts import redirect
 from .models import Account
-class Balance(TemplateView):
+from django.contrib.auth.mixins import LoginRequiredMixin
+class Balance(LoginRequiredMixin,TemplateView):
     template_name='accounts/balance.html'
 
-def debit(request):
-    amount=request.POST['amount']
-    if amount!='':
-        amount=int(request.POST['amount'])
-        my_account=Account.objects.get(owner=request.user)
-        print(my_account)
-        print("before : ",my_account.balance)
-        if amount>my_account.balance:
-            print(my_account.balance-amount)
-            print("No Sufficient balance")
-        else:
-            my_account.balance-=amount
-        my_account.save()
-        print("After ",my_account.balance)
-    return redirect('account')
+class Debit(LoginRequiredMixin):
+    def debit(request):
+        amount=request.POST['amount']
+        if amount!='':
+            amount=int(request.POST['amount'])
+            my_account=Account.objects.get(owner=request.user)
+            print(my_account)
+            print("before : ",my_account.balance)
+            if amount>my_account.balance:
+                print(my_account.balance-amount)
+                print("No Sufficient balance")
+            else:
+                my_account.balance-=amount
+            my_account.save()
+            print("After ",my_account.balance)
+        return redirect('account')
 
-def credit(request):
-    amount=request.POST['amount']
-    if amount!='':
-        amount=int(request.POST['amount'])
+class Credit(LoginRequiredMixin):
+    def credit(request):
+        amount=request.POST['amount']
+        if amount!='':
+            amount=int(request.POST['amount'])
+            my_account=Account.objects.get(owner=request.user)
+            print(my_account)
+            print("before : ",my_account.balance)
+            my_account.balance+=amount
+            my_account.save()
+            print("After ",my_account.balance)
+        return redirect('account')
+class Balance(LoginRequiredMixin):
+    def balance(request):
         my_account=Account.objects.get(owner=request.user)
-        print(my_account)
-        print("before : ",my_account.balance)
-        my_account.balance+=amount
-        my_account.save()
-        print("After ",my_account.balance)
-    return redirect('account')
-
-def balance(request):
-    my_account=Account.objects.get(owner=request.user)
-    return render(request,'accounts/balance.html',{'balance':my_account.balance})
+        return render(request,'accounts/balance.html',{'balance':my_account.balance})
