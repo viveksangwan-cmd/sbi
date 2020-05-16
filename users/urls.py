3c9be8a28504f7f6aa7b5011803654d450294c10
vivek <viveksangwan006@gmaiil.com>
2020-05-16 20:25:23 +0530

    design

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-    path('signup/',SignUpView.as_view(),name='signup'),
+    path('signup/',SignUpView.signup_view,name='signup'),
     path('detail/',UserDetailView.as_view(),name='customerdetail'),
     path('',TemplateView.as_view(template_name='customer.html'),name='customer'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,18 +1,25 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,redirect
 from django.views.generic import CreateView,DetailView
 from .forms import CustomUserCreationForm
 from django.urls import reverse_lazy
 from .models import CustomUser
 from django.db import connection
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login,authenticate
 
-class SignUpView(CreateView):
-    form_class=CustomUserCreationForm
-    success_url=reverse_lazy('login')
-    template_name='signup.html'
+class SignUpView(LoginRequiredMixin):
+    def signup_view(request):
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')
+        return render(request, 'signup.html', {'form': form})
 
-
-class UserDetailView(DetailView):
+class UserDetailView(LoginRequiredMixin,DetailView):
     model=CustomUser
     template_name='customerDetail.html'
 
